be206793946a9cd0f460f243db66d45639b50a65
Jörg Steffen <steffen@dfki.de>
2017-06-23 17:22:00 +0200

    Fix StringUtils import

--- src/main/java/com/gn/performance/UDlanguagePerformance.java
+++ src/main/java/com/gn/performance/UDlanguagePerformance.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import de.dfki.mlt.gnt.data.Pair;
 
